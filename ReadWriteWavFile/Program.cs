7444f0158c9033a65b03330f368986a1e3322e05
Adnan Zafar <45895341+AdnanZafar0005@users.noreply.github.com>
2022-08-17 03:10:45 +0500

    Add project files.

---
+++ ReadWriteWavFile/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using ReadWriteWavFile;
+using WavFile;
+
+        string filePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\Files\\", "WavFile.wav");
+        if (!String.IsNullOrEmpty(filePath))
+        {
+            // Read Header
+            WavFileHeader header = ReadWriteFile.ReadWavFile(filePath);        
+
+            // Read Data 
+            byte[] dataArray = ReadWriteFile.ReadWavData(filePath);
+            Console.WriteLine("Displaying Data");
+            Console.WriteLine(header);
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Displaying Data");
+
+            if (dataArray.Length > 0)
+            {
+                foreach (var data in dataArray)
+                {
+                    Console.Write(data);
+                }
+            }
+
+            // Write channel files
+            double channelData = ReadWriteFile.WriteWavFile(filePath);
+            Console.WriteLine(channelData);
+            Console.WriteLine("Channel Files created.\n");
+
+             // Creating csv file
+            string csvfilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\Files\\Csv\\", "WavFile.csv");
+            ReadWriteFile.CreateCsvFile(csvfilePath);
+
+            Console.WriteLine("Csv file created and channel name and data written.\n");
+        }
+
+        Console.ReadKey();
---
+++ ReadWriteWavFile/ReadWriteFile.cs
@@ -0,0 +1,77 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WavFile;
+
+namespace ReadWriteWavFile;
+
+public static class ReadWriteFile
+{
+
+    public static WavFileHeader ReadWavFile(string fileName)
+    {
+        WavFileHeader header = new WavFileHeader();
+        using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+        {
+            header = WavFileInfo.ReadFileHeader(fs);
+        }
+        return header;
+    }
+
+    public static byte[] ReadWavData(string filePath)
+    {
+        byte[] data = File.ReadAllBytes(filePath);
+        return data;
+    }
+
+    public static double WriteWavFile(string fileName)
+    {
+        string outputPath = string.Empty;
+        outputPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\Files\\ChannelWavFiles\\";
+          
+        double bytesTotal = 0;
+        try
+        {
+                var splitter = new WavFileSplitter(value => { });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        return bytesTotal;
+    }
+
+    public static void CreateCsvFile(string csvPath)
+    {
+        string channelFilesPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\Files\\ChannelWavFiles\\";
+
+        List<string> files = Directory.EnumerateFiles(channelFilesPath, "*.wav").ToList();
+        List<CsvMappingModel> recordsToAdd = new List<CsvMappingModel>();
+
+        foreach (string filePath in files)
+        {
+            string filename = Path.GetFileName(filePath);
+            var data = File.ReadAllLines(filePath);
+            var record = new CsvMappingModel()
+            {
+                Name = filename,
+                Data = string.Join("", data)
+            };
+            recordsToAdd.Add(record);
+        }
+
+        //Writing Channel file name and data into csv
+        using (var writer = new StreamWriter(csvPath))
+        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(recordsToAdd);
+        }
+    }
+}
